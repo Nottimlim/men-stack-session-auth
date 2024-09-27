23981eb83935b357dd7f3ce88e267264d4e660ec
Timothy Lim <contacttimothylim@gmail.com>
2024-09-27 14:20:23 -0400

    commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
-
+const User = require("../models/user.js");
 module.exports = router;
 
-router.get("/sign-up", (req, res) => {
+router.get("/sign-up", async (req, res) => {
     res.render("auth/sign-up.ejs");
+    const userInDatabase = await User.findOne({ username: req.body.username });
+if (userInDatabase) {
+  return res.send("Username already taken.");
+}
   });
   
+  router.post("/sign-up", async (req, res) => {
+    res.send("Form submission accepted!");
+  });
