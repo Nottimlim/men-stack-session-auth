5c091dc2ec25417d6800e6312ae7894a6915c209
Timothy Lim <contacttimothylim@gmail.com>
2024-09-27 15:52:38 -0400

    commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,23 +6,52 @@ module.exports = router;
 
 router.get("/sign-up", async (req, res) => {
     res.render("auth/sign-up.ejs");
-    const userInDatabase = await User.findOne({ username: req.body.username });
-    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
-    const user = await User.create(req.body);
-    es.send(`Thanks for signing up ${user.username}`);
-    req.body.password = hashedPassword;
-    if (userInDatabase) {
-    return res.send("Username already taken.");
-    if (req.body.password !== req.body.confirmPassword) {
-    return res.send("Password and Confirm Password must match");
-  }
-}});
+  });
   
   router.post("/sign-up", async (req, res) => {
-    res.send("Form submission accepted!");
+    const userInDatabase = await User.findOne({ username: req.body.username });   
+    
+    if (userInDatabase) {
+    return res.send("Username already taken.");
+    }
+
+    if (req.body.password !== req.body.confirmPassword) {
+    return res.send("Password and Confirm Password must match");
+    }
+
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+    req.body.password = hashedPassword;
+    const user = await User.create(req.body);
+    res.send(`Thanks for signing up ${user.username}`);
+    
   });
 
   router.get("/sign-in", (req, res) => {
     res.render("auth/sign-in.ejs");
+  });
+  
+  router.post("/sign-in", async (req, res) => {
+    const userInDatabase = await User.findOne({ username: req.body.username });
+    if (!userInDatabase) {
+    return res.send("Login failed. Please try again."); };
+
+    const validPassword = bcrypt.compareSync(
+        req.body.password,
+        userInDatabase.password
+      );
+    if (!validPassword) {
+        return res.send("Login failed. Please try again.");
+      }
+    req.session.user = {
+        username: userInDatabase.username,
+        _id: userInDatabase._id
+    };
+    
+    res.redirect("/");
+});
+  
+  router.get("/sign-out", (req, res) => {
+    req.session.destroy();
+    res.redirect("/");
   });
   
